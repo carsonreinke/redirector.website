28eea350f17cdf94b93f872a27877f216d747298
Carson Reinke <carson@reinke.co>
2019-07-07 09:12:50 -0400

    Swap dynamic float IP with just a fixed one

--- ansible/lets-encrypt-cleanup.sh
+++ ansible/lets-encrypt-cleanup.sh
@@ -3,7 +3,8 @@
 $(which certbot) certificates | grep --color=none -o -P "(?<=Domains: )(.+)" | while IFS= read -r DOMAIN; do
     #Digital Ocean specific method to retrieve floating IP address
     #See https://www.digitalocean.com/docs/networking/floating-ips/how-to/find-anchor-ips/
-    FLOATING_IP=$(curl -s http://169.254.169.254/metadata/v1/floating_ip/ipv4/ip_address | grep --color=never -o -P "([0-9\.]+)" | tr -d "\n")
+    #FLOATING_IP=$(curl -s http://169.254.169.254/metadata/v1/floating_ip/ipv4/ip_address | grep --color=never -o -P "([0-9\.]+)" | tr -d "\n")
+    FLOATING_IP="45.55.123.140"
 
     #Get the IP address 
     DOMAIN_IP=$(host -t a $DOMAIN | grep --color=never -o -P "(?<=has address )([0-9\.]+)" | tr -d "\n")
--- ansible/lets-encrypt-create.sh
+++ ansible/lets-encrypt-create.sh
@@ -4,7 +4,8 @@
 cat /var/log/nginx/access.log | sort | uniq | while IFS= read -r DOMAIN; do
     #Digital Ocean specific method to retrieve floating IP address
     #See https://www.digitalocean.com/docs/networking/floating-ips/how-to/find-anchor-ips/
-    FLOATING_IP=$(curl -s http://169.254.169.254/metadata/v1/floating_ip/ipv4/ip_address | grep --color=never -o -P "([0-9\.]+)" | tr -d "\n")
+    #FLOATING_IP=$(curl -s http://169.254.169.254/metadata/v1/floating_ip/ipv4/ip_address | grep --color=never -o -P "([0-9\.]+)" | tr -d "\n")
+    FLOATING_IP="45.55.123.140"
 
     #Get the IP address 
     DOMAIN_IP=$(host -t a $DOMAIN | grep --color=never -o -P "(?<=has address )([0-9\.]+)" | tr -d "\n")
